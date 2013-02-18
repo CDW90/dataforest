e2345813c27380e7ce79ff4d48a3293b6cf68fa5
blackearth7 <oranienbuger@gmail.com>
2013-02-18 19:55:22 +0100

    OpenFileDialogKlasse hinzugefügt, weiß aber nicht wie ich per xaml darauf zugreifen kann..

---
+++ src/DataForest/ViewModel/OpenFileDialogVM.cs
@@ -0,0 +1,52 @@
+﻿using DataForest.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace DataForest.ViewModel
+{
+    class OpenFileDialogVM : BaseViewModel
+    {
+        public static RelayCommand OpenCommand { get; set; }
+
+        private string _selectedPath;
+        public string SelectedPath
+        {
+            get { return _selectedPath; }
+            set
+            {
+                _selectedPath = value;
+                OnPropertyChanged("SelectedPath");
+            }
+        }
+
+        private string _defaultPath;
+
+        public OpenFileDialogVM()
+        {
+            RegisterCommands();
+        }
+
+        public OpenFileDialogVM(string defaultPath)
+        {
+            _defaultPath = defaultPath;
+            RegisterCommands();
+        }
+
+        private void RegisterCommands()
+        {
+            OpenCommand = new RelayCommand(param => ExecuteOpenFileDialog());
+        }
+
+        private void ExecuteOpenFileDialog()
+        {
+            var dialog = new OpenFileDialog { InitialDirectory = _defaultPath };
+            dialog.ShowDialog();
+
+            SelectedPath = dialog.FileName;
+        }
+    }
+}
